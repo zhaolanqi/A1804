ba0a4a5f191fd112f20b0d2bc4c0b34c4ee221c9
zhaolanqi <oava126074@163.com>
2018-05-08 18:23:27 +0800

    xiajierxeiw

--- zhaoying.py
+++ zhaoying.py
@@ -4,27 +4,27 @@ b=10
 c=0
 
 c=a+b
-print("1-c:",c)
+print("1-c的值为:",c)
 
 c=a-b
-print("2-c:",c)
+print("2-c的值为:",c)
 
 c=a*b
-print("3-c:",c)
+print("3-c的值为:",c)
 
 c=a/b
-print("4-c:",c)
+print("4-c的值为:",c)
 
 c=a%b
-print("5-c:",c)
+print("5-c的值为:",c)
 
 #修改变量 a,b,c
 a=2
 b=3
 c=a**b
-print("6-c:",c)
+print("6-c的值为:",c)
 
 a=10
 b=5
 c=a//b
-print("7-c:",c)
+print("7-c的值为:",c)
